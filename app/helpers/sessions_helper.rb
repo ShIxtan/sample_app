42425772150ed2a1f77bf7386e4c92b6dde43f0c
ShIxtan <aulbrich@iu.edu>
2014-11-24 22:11:37 -0500

    fix bug with logout over multiple tabs/browsers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,12 +23,20 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
