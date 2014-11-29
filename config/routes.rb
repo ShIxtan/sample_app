18ed6cd7a78a94602a0918d14000346d4db75c2b
ShIxtan <aulbrich@iu.edu>
2014-11-28 21:49:01 -0500

    add activation digest to database

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
+  resources :account_activations, only: [:edit]
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
